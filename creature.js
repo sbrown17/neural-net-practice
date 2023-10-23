01e70e129ea3611b119d85b436f253e3cdfa0637
Stu <stubrown357@gmail.com>
2023-10-22 21:23:47 -0500

    add creture class

---
+++ creature.js
@@ -0,0 +1,12 @@
+class Creature {
+    constructor(x, y, r) {
+    this.position = new p5.Vector(x, y);
+    this.velocity = p5.Vector.random2D();
+    this.velocity.mult(3);
+    this.r = r;
+    this.m = r * 0.1;
+  }
+  update() {
+    this.position.add(this.velocity);
+  }
+}
