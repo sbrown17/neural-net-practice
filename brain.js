3454980ea1913b8f118d2fd87f65e52f1fbc0b67
Stu <stubrown357@gmail.com>
2023-10-29 10:05:29 -0500

    summing output weights by connection

--- brain.js
+++ brain.js
@@ -1,8 +1,25 @@
 export default class Brain {
-    constructor(input_nodes, hidden_nodes, ouput_nodes){
+    constructor(input_nodes, input_weights, hidden_nodes, output_nodes){
         this.input_nodes = input_nodes;
+        this.weights = input_weights;
         this.hidden_nodes = hidden_nodes;
-        this.ouput_nodes = ouput_nodes;
+        this.output_nodes = output_nodes;
     }
+
     //ninput nodes based off environment
+    feedForward(input_nodes, output_nodes) {
+       // need to know:
+       // input_node {
+       //   output_pointers[0..n]
+       //   connection_weight[] array of floats from -1.0 to 1.0, same number of weights as connection_pointers
+       // }
+       // output_nodes => array of floats (all connection strings to it summed)
+        let outputNodeWeights;
+        forEach.input_nodes(node => {
+            
+            for (let i = 0; i < node.output_pointers.length; i++){
+                outputNodeWeights[node.output_pointers[i]] += connection_weight[i];
+            }
+        });
+    }
 }
