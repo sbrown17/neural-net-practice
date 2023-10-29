7dae32560cb16111eb5e1ca793c95d618291a2c7
Stuart Brown <stubrown357@gmail.com>
2023-10-29 20:12:30 +0000

    remove comments

--- brain.js
+++ brain.js
@@ -6,14 +6,7 @@ export default class Brain {
         this.output_nodes = output_nodes;
     }
 
-    //ninput nodes based off environment
     feedForward(input_nodes, output_nodes) {
-       // need to know:
-       // input_node {
-       //   output_pointers[0..n]
-       //   connection_weight[] array of floats from -1.0 to 1.0, same number of weights as connection_pointers
-       // }
-       // output_nodes => array of floats (all connection strings to it summed)
         let outputNodeWeights;
         forEach.input_nodes(node => {
             
