e18bc3a47c299b1449c6440884b560086d0e67a2
Stuart Brown <stubrown357@gmail.com>
2023-10-29 20:45:56 +0000

    refactor brain INTO the creature class

--- creature.js
+++ creature.js
@@ -1,13 +1,26 @@
 
 export default class Creature {
-  constructor(x, y, r) {
+  constructor(input_nodes, x, y) {
     this.position = new p5.Vector(x, y);
-    this.velocity = p5.Vector.random2D();
-    this.velocity.mult(3);
-    this.r = r;
+    this.velocity = outputWeights[0];
+    this.r = outputWeights[1];
     this.m = r * 0.1;
+    this.input_nodes = input_nodes;
+    this.
   }
+  
   update() {
     this.position.add(this.velocity);
   }
+  
+  feedForward(input_nodes) {
+        let outputNodeWeights;
+        forEach.input_nodes(node => {
+            
+            for (let i = 0; i < node.output_pointers.length; i++){
+                outputNodeWeights[node.output_pointers[i]] += connection_weight[i];
+            }
+       });
+       return outputNodeWeights;
+    }
 }
