35de22fcb77488e2a50e993672728217213212f5
Io <stubrown357@gmail.com>
2024-02-20 20:50:20 -0600

    remove unnecessary comments

--- creature.js
+++ creature.js
@@ -1,10 +1,10 @@
 export class Creature {
   constructor(outputFeed, x, y) {
-    this.position = new p5.Vector(x, y); // x: 200, y: 200, z: 0
-    this.outputNodeWeights = outputFeed; // empty []
-    this.velocity = this.outputNodeWeights[0]; // undefined
-    this.r = this.outputNodeWeights[1]; // undefined
-    this.m = this.r * 0.1; // NaN
+    this.position = new p5.Vector(x, y);
+    this.outputNodeWeights = outputFeed;
+    this.velocity = this.outputNodeWeights[0];
+    this.r = this.outputNodeWeights[1];
+    this.m = this.r * 0.1;
     this.color = Math.floor(Math.random() * 999);
   }
   
